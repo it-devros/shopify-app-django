153508bda36248828c1669c684f0f1e0dd436bac
it-devros <it-devros@outlook.com>
2018-02-04 20:51:17 -0500

    fixing

--- main_app/static/script/front.js
+++ main_app/static/script/front.js
@@ -17,7 +17,7 @@ var CremaScript = (function () {
 
         if (window.localStorage.getItem("load_front") == "no") {
             // var obj = document.getElementById("text_pannel");
-            vr obj = document.querySelector("itemprop");
+            vr obj = this.getAllElementsWithAttribute("itemprop", "description");
             console.log("++++++++++++++ obj +++++++++++++++", obj);
             // if (obj) {
             //     var str = "<h3 style='color:" + this.Options.font_color + ";font-weight:" + this.Options.font_weight + ";font-size:" + this.Options.font_size + ";'>" + this.Options.content + "&nbsp;&nbsp;&nbsp;&nbsp;" + this.Options.days + " Days" + "&nbsp;&nbsp;" + this.Options.hours + " Hours" + "&nbsp;&nbsp;" + this.Options.mins + " Minutes" + "&nbsp;&nbsp;" + this.Options.secs + " Seconds" + "&nbsp;&nbsp;" + "Left! Hurry Up." + "</h3>";
@@ -26,6 +26,22 @@ var CremaScript = (function () {
             // window.localStorage.setItem("load_front", "yes");
         }
 
+    }
+
+    function getAllElementsWithAttribute(attribute, value)
+    {
+      var matchingElements = [];
+      var allElements = document.getElementsByTagName('*');
+      for (var i = 0, n = allElements.length; i < n; i++)
+      {
+        if (allElements[i].getAttribute(attribute) !== null)
+        {
+          // Element exists with attribute. Add to array.
+          console.log("++++++++++++ matched ++++++++++++", allElements[i].itemprop);
+          matchingElements.push(allElements[i]);
+        }
+      }
+      return matchingElements;
     }
 
 
