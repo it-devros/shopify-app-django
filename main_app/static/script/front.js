8fdd2a6cbcf5a3d5260d52b51c9000820f120749
it-devros <it-devros@outlook.com>
2018-02-04 20:26:02 -0500

    fixing

--- main_app/static/script/front.js
+++ main_app/static/script/front.js
@@ -16,16 +16,15 @@ var CremaScript = (function () {
         this.Options = this.ConfigureDefaults(Options);
 
         if (window.localStorage.getItem("load_front") == "no") {
-            var obj = document.getElementById("text_pannel");
+            // var obj = document.getElementById("text_pannel");
+            vr obj = document.querySelector("div[itemprop='description']");
             console.log("++++++++++++++ obj +++++++++++++++", obj);
             if (obj) {
                 var str = "<h3 style='color:" + this.Options.font_color + ";font-weight:" + this.Options.font_weight + ";font-size:" + this.Options.font_size + ";'>" + this.Options.content + "&nbsp;&nbsp;&nbsp;&nbsp;" + this.Options.days + " Days" + "&nbsp;&nbsp;" + this.Options.hours + " Hours" + "&nbsp;&nbsp;" + this.Options.mins + " Minutes" + "&nbsp;&nbsp;" + this.Options.secs + " Seconds" + "&nbsp;&nbsp;" + "Left! Hurry Up." + "</h3>";
-                obj.insertAdjacentHTML("beforeend", str);
+                obj.insertAdjacentHTML("afterstart", str);
             }
             window.localStorage.setItem("load_front", "yes");
         }
-
-        var ok = "this is ok";
 
     }
 
